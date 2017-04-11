ecd4e203e5300d210e86ff9c9b95bb63f470e667
JoseFlavioAssisJr <joseflaviojr@outlook.com>
2017-04-10 21:17:39 -0300

    Adiciona modelo de dados do sql

---
+++ Aula10_04/src/java/sql/Contato_tabela.sql
@@ -0,0 +1,10 @@
+CREATE TABLE contato
+(
+id INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS
+IDENTITY,
+nome VARCHAR(50) NOT NULL,
+sobrenome VARCHAR(90) NOT NULL,
+telefone VARCHAR(20) NOT NULL
+);
+
+INSER INTO contato (nome, sobrenome, telefone) VALUES ('Fulano', 'da Silva', '555-5555');
