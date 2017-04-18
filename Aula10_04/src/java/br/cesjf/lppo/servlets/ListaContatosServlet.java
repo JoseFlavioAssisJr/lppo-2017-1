db495c330321af30f23532adcd215048253f62cd
aluno <aluno@L5MAQ13.labcesac.net>
2017-04-17 22:19:03 -0300

    Lista ContatoServlet atualizado

--- Aula10_04/src/java/br/cesjf/lppo/servlets/ListaContatosServlet.java
+++ Aula10_04/src/java/br/cesjf/lppo/servlets/ListaContatosServlet.java
@@ -3,7 +3,6 @@ package br.cesjf.lppo.servlets;
 import br.cesjf.lppo.Contato;
 import br.cesjf.lppo.dao.ContatoDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -13,7 +12,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import sun.util.logging.PlatformLogger;
 
 /**
  *
